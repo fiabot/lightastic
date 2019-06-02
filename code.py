866f1c6aecb761d8b25de56b05a3e4fdccacffb0
fiabot <fiona.shyne@gmail.com>
2019-06-02 07:51:01 -0500

    Update code.py
    fixed brightness

--- code.py
+++ code.py
@@ -21,7 +21,7 @@ class NeopixelPattern:
          self.green,self.green_blue,self.blue,self.blue_purple,self.purple,self.red_purple]
          #current frame number
          self.frame = 0
-
+         
          #wait times for animation
          self.smooth_time = 0.01
          #self.pulse_time_start = 0.4
@@ -34,15 +34,15 @@ class NeopixelPattern:
 
          #automatic pattern type
          self.auto_patt = "fism" #spin smooth
-
-         #solid pattern
-         self.color_count = 0
-         self.num_colors = 10
-         self.num_steps = self.transition_time/ self.smooth_time
-         self.cur_step = 0
-         self.cur_brightness = self.brightness
-
-         self.fade_in = False
+         
+         #solid pattern 
+         self.color_count = 0 
+         self.num_colors = 10 
+         self.num_steps = self.transition_time/ self.smooth_time 
+         self.cur_step = 0 
+         self.cur_brightness = self.brightness 
+         
+         self.fade_in = False 
 
 
      """def show_base_frame(self):
@@ -84,71 +84,71 @@ class NeopixelPattern:
          time.sleep(self.pulse_time_start)
          cpx.pixels.fill((0,0,0))
          time.sleep(self.pulse_time_end)
-
-     def display_fill_smooth(self, random = False):
-         if self.color_count == (self.num_colors-1):
+     
+     def display_fill_smooth(self, random = False): 
+         if self.color_count == (self.num_colors-1): 
              r_dif = (self.colors[self.color_count][0] - self.colors[0][0])\
-              / self.num_steps * self.cur_step
+              / self.num_steps * self.cur_step 
              g_dif = (self.colors[self.color_count][1] - self.colors[0][1])\
-                  / self.num_steps * self.cur_step
+                  / self.num_steps * self.cur_step 
              b_dif = (self.colors[self.color_count][2] - self.colors[0][2])\
                   / self.num_steps * self.cur_step
-         else:
+         else: 
              r_dif = (self.colors[self.color_count][0] - self.colors[self.color_count+1][0])\
-                  / self.num_steps * self.cur_step
+                  / self.num_steps * self.cur_step 
              g_dif = (self.colors[self.color_count][1] - self.colors[self.color_count+1][1])\
-                  / self.num_steps * self.cur_step
+                  / self.num_steps * self.cur_step 
              b_dif = (self.colors[self.color_count][2] - self.colors[self.color_count+1][2])\
-                  / self.num_steps * self.cur_step
-
+                  / self.num_steps * self.cur_step 
+         
          r = int(self.colors[self.color_count][0] - r_dif)
          g = int(self.colors[self.color_count][1] - g_dif)
          b = int(self.colors[self.color_count][2] - b_dif)
          color = (r,g,b)
-
+        
          #color = self.colors[self.color_count]
-
-         cpx.pixels.fill(color)
+         
+         cpx.pixels.fill(color) 
          self.cur_step += 1
-         if self.cur_step > self.num_steps:
-             self.cur_step = 0
-             self.color_count += 1
-
-             if self.color_count >= (self.num_colors):
-                 self.color_count = 0
+         if self.cur_step > self.num_steps: 
+             self.cur_step = 0 
+             self.color_count += 1 
+             
+             if self.color_count >= (self.num_colors): 
+                 self.color_count = 0 
          time.sleep(self.smooth_time)
-
-     def display_fill_pulse(self, random = False):
-         color = self.colors[self.color_count]
+    
+     def display_fill_pulse(self, random = False): 
+         color = self.colors[self.color_count] 
          cpx.pixels.brightness = self.cur_brightness
          """ r = int(color[0]/(1-self.cur_brightness))
          g= int(color[1]/(1-self.cur_brightness))
          b = int(color[2]/(1-self.cur_brightness))
          color =(r,g,b)"""
-         cpx.pixels.fill(color)
-
+         cpx.pixels.fill(color) 
+         
          step = (self.brightness)/ self.num_steps
-
-         if self.fade_in:
-             self.cur_brightness += step
-         else:
-             self.cur_brightness -= step
-
-
+         
+         if self.fade_in: 
+             self.cur_brightness += step 
+         else: 
+             self.cur_brightness -= step 
+              
+             
          self.cur_step += 1
-         if self.cur_step > self.num_steps:
+         if self.cur_step > self.num_steps: 
              print(self.cur_brightness)
-             self.cur_step = 0
-
-             self.fade_in = not self.fade_in
+             self.cur_step = 0 
+             
+             self.fade_in = not self.fade_in 
              if self.fade_in:
-                self.color_count += 1
-
-             if self.color_count >= (self.num_colors):
+                self.color_count += 1 
+             
+             if self.color_count >= (self.num_colors): 
                  self.color_count = 0
-
+             
          time.sleep(self.smooth_time)
-
+         
      def display_auto(self):
          if self.auto_patt == "spsm":
              self.display_spin_smooth()
@@ -159,9 +159,9 @@ class NeopixelPattern:
          elif self.auto_patt == "rapu":
              self.display_rand_pulse()
          elif self.auto_patt == "fism":
-             self.display_fill_smooth()
-         else:
-             self.display_fill_pulse()
+             self.display_fill_smooth() 
+         else: 
+             self.display_fill_pulse() 
 
 
 fire = NeopixelPattern()
@@ -176,7 +176,7 @@ siren = NeopixelPattern()
 siren.colors = (siren.blue,siren.red)
 siren.num_colors = 2
 siren.smooth_time = 0.0001
-siren.num_steps = 0.0005 / siren.smooth_time
+siren.num_steps = 0.0007 / siren.smooth_time
 siren.auto_patt = "fipu"
 
 cool = NeopixelPattern()
@@ -192,11 +192,11 @@ party.num_steps = 0.0005 / party.smooth_time
 
 pattern_list = [rainbow,fire,siren, cool,party]
 
-pattern = 3
+pattern = 0
 while True:
      pattern_list[pattern].display_auto()
      if cpx.tapped:
         pattern += 1
+        cpx.pixels.brightness = 0.3
         if pattern >= 5:
-            cpx.pixels.brightness = 0.3
             pattern = 0
